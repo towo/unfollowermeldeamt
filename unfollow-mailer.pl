c3f8432cab89bf634385f7054939fe510fce04b1
towo <towo@ydal.de>
2012-10-23 15:28:52 +0200

    Initial import

---
+++ unfollow-mailer.pl
@@ -0,0 +1,133 @@
+#!/usr/bin/perl
+# mails log of people who unfollowed the authenticated user
+
+# Copyright 2012 Tobias Wolter
+
+# MIT licence:
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+use Config::Simple;
+use Net::Twitter;
+use Mail::Sendmail;
+use strict;
+
+# config file
+my $rcfile = "$ENV{HOME}/.unfollow-mailerrc";
+# config container
+my $config;
+
+if (-r $rcfile) {
+	$config = new Config::Simple;
+	$config->read($rcfile);
+} else {
+	if (-e $rcfile) {
+		die "$0: configuration file «$rcfile» not readable\n";
+	} else {
+		$config = new Config::Simple('syntax' => 'ini');
+		foreach my $variable (qw{oauth.consumer_key oauth.consumer_secret oauth.access_token oauth.access_token_secret mail.target mail.source twitter.follower_ids twitter.last_check}) {
+			$config->param($variable, '');
+		}
+		$config->write($rcfile);
+		print "$0: Blank configuration file «$rcfile» created.\n";
+		print "    Please edit the file to supply the necessary information.\n";
+		print "    You will need to create your own Twitter application;\n";
+		print "    this can be done at https://dev.twitter.com/\n";
+		exit 0;
+	}
+}
+
+# fluff
+
+my $mailheader  = "Hello, this is your friendly Twitter monitoring script thingie!\n\nThe following users unfollowed you";
+if ($config->param('twitter.last_check')) {
+   $mailheader .= " since I was last run on " . $config->param('twitter.last_check');
+}
+   $mailheader .= ":\n\n";
+my $mailfooter  = "Burn in rage as you curse their ancestors!\n\nHave a nice day!\n";
+
+# FIXME: config sanity checks
+my $twitter = Net::Twitter->new(
+	consumer_key        => $config->param('oauth.consumer_key'),
+	consumer_secret     => $config->param('oauth.consumer_secret'),
+	access_token        => $config->param('oauth.access_token'),
+	access_token_secret => $config->param('oauth.access_token_secret'),
+	traits              => [qw/OAuth API::REST/],
+);
+
+# FIXME: exception handling
+my %seen;
+# loops for followers; taken mostly straight from Net::Twitter perldoc
+for (my $cursor = -1, my $r; $cursor; $cursor = $r->{next_cursor}) {
+	$r = $twitter->followers_ids({ cursor => $cursor });
+	foreach (@{$r->{ids}}) {
+		$seen{$_} = 1;
+	}
+}
+
+my @unfollowing_ids;
+foreach (@{$config->param('twitter.follower_ids')}) {
+	if ($seen{$_} != 1) {
+		push(@unfollowing_ids, "$_");
+	}
+}
+
+my $number_unfollowers = scalar @unfollowing_ids;
+
+# FIXME: pagination
+if ($number_unfollowers > 100) {
+	die "$0: pagination for more than 100 user lookups not implemented yet, bailing out.\n";
+}
+
+# no unfollowers
+if ($number_unfollowers == 0) {
+	exit 0;
+}
+
+my %unfollowers; my $data;
+$data = $twitter->lookup_users(user_id => \@unfollowing_ids);
+
+foreach (@{$data}) {
+	$unfollowers{$_->{screen_name}} = $_->{name};
+}
+
+my $subject  = "Twitter: ";
+   $subject .= ($number_unfollowers == 1) ? "one person " : "$number_unfollowers people ";
+   $subject .= "unfollowed you!";
+
+my $message  = '';
+map { $message .= "  * $unfollowers{$_} ($_)\n" } sort(keys(%unfollowers));
+   $message .= "\n";
+
+my %mail = (
+	To      => $config->param('mail.target'),
+	From    => $config->param('mail.source'),
+	Message => "${mailheader}${message}${mailfooter}",
+	Subject => $subject
+);
+
+sendmail(%mail) or die $Mail::Sendmail::error;
+
+my $seen_string = join(',', sort keys %seen);
+
+# write seen users to configuration
+$config->param('twitter.follower_ids', $seen_string);
+
+# log last check time
+$config->param('twitter.last_check', scalar localtime);
+$config->save;
